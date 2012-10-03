53fc2a44fa6914ec4dd61b9fe10c7c0693a47ede
Oren Yagev <oren@onehourtranslation.com>
2012-10-03 10:30:27 +0200

    Build the DOM

--- src/XliffDocument.php
+++ src/XliffDocument.php
@@ -1,16 +1,33 @@
 <?php
 
-abstract class XliffElement{
+class XliffNode{
 	protected $attributes = array();
 	protected $containers = array();
 	protected $supportedContainers = array();
 	
-	function __construct(){
+	protected $nodes = array();
+	protected $supportedNodes = array();
+	
+	protected $textContent=NULL;
+	
+	protected $name = '';
+	
+	function __construct($name=NULL){
+		if($name) $this->setName($name);
 		foreach($this->supportedContainers as $name=>$class){
 			$this->containers[$name] = array();
 		}
 	}
 	
+	public function getName()
+	{
+	    return $this->name;
+	}
+
+	public function setName($name)
+	{
+	    $this->name = $name;
+	}
 	function getAttribute($name){
 		return (isset($this->attributes[$name])) ? $this->attributes[$name] : FALSE;
 	}
@@ -21,6 +38,19 @@ abstract class XliffElement{
 		$this->attributes[$name] = trim((string)$value);
 		return $this;
 	}
+	
+	public function getTextContent()
+	{
+	    return $this->textContent;
+	}
+
+	public function setTextContent($textContent)
+	{
+	    $this->textContent = $textContent;
+	    return $this;
+	}
+	
+	
 	
 	/**
 	 * Support calling $obj->container($new=FALSE)
@@ -43,18 +73,58 @@ abstract class XliffElement{
 				
 			}
 			return end($this->containers[$pluralName]);
+			
+		}elseif(!empty($this->supportedNodes[$name])){
+			//Check if node already created
+			if (empty($this->nodes[$name])){
+				//create new node on-the-fly
+				$cls = $this->supportedNodes[$name];
+				
+				$this->nodes[$name] = new $cls();
+				$this->nodes[$name]->setName($name);
+			}
+			
+			return $this->nodes[$name];
 		}
-		throw new Exception("'$name' is unknown");
+		throw new Exception(sprintf("'%s' is not supported for '%s'",$name,get_class($this)));
 	}
+	
+	function toDOMElement(DOMDocument $doc){
+		$element = $doc->createElement($this->getName());
+		foreach($this->attributes as $name=>$value){
+			$element->setAttribute($name, $value);
+		}
+		foreach($this->containers as $container){
+			foreach($container as $node){
+				$element->appendChild($node->toDOMElement($doc));
+			}
+		}
+		foreach($this->nodes as $node){
+			$element->appendChild($node->toDOMElement($doc));
+		}
+		if ($text = $this->getTextContent()){
+			$textNode = $doc->createTextNode($text);
+			$element->appendChild($textNode);
+		}
+		return $element;
+	}
+	
+
 	
 } 
 
-class XliffDocument extends XliffElement{
+/**
+ * Enter description here ...
+ * @author oyagev
+ * @method XliffFile file() file()
+ */
+class XliffDocument extends XliffNode{
 	/**
      * uncomplete xliff Namespace
      */
     const NS = 'urn:oasis:names:tc:xliff:document:';
     
+    protected $name = 'xliff';
     protected $supportedContainers = array(
     	'files' => 'XliffFile',
     );
@@ -79,17 +149,19 @@ class XliffDocument extends XliffElement{
     }
     
    
-    protected function createDomTree(){
+    public function toDOM(){
     	// create the new document
     	$doc = new DOMDocument();
 
         // create the xliff root element
-        $xliff = $doc->createElement('xliff');
+        $xliff = $this->toDOMElement($doc);
         // little hack to workaround the unusable php namespace handling
         $xliff->setAttribute('xmlns', self::NS . $this->version);
         // add the xliff version
         $xliff->setAttribute('version',$this->version);
+        //$xliff->appendChild($this->toDOMElement($doc));
         $doc->appendChild($xliff);
+        return $doc;
         
     }
     
@@ -98,25 +170,62 @@ class XliffDocument extends XliffElement{
 }
 
 
-class XliffFile extends XliffElement{
+/**
+ * Enter description here ...
+ * @author oyagev
+ * @method XliffFileBody body()
+ * @method XliffFileHeader header()
+ */
+class XliffFile extends XliffNode{
+	protected $name = 'file';
+	protected $supportedNodes = array(
+		'header' 	=> 'XliffFileHeader',
+		'body' 		=> 'XliffFileBody',
+	);
+	
+}
+
+class XliffFileHeader extends XliffNode{
+	protected $name = 'header';
+}
+/**
+ * Enter description here ...
+ * @author oyagev
+ * @method XliffUnitsGroup group()
+ * @method XliffUnit unit()
+ * @method array groups()
+ * @method array units()
+ */
+class XliffFileBody extends XliffNode{
+	protected $name = 'body';
 	protected $supportedContainers = array(
     	'groups'	=> 'XliffUnitsGroup',
 		'units'		=> 'XliffUnit'
     );
 }
 
-class XliffFileHeader extends XliffElement{
-	
-}
 
-class XliffUnitsGroup extends XliffElement{
+class XliffUnitsGroup extends XliffNode{
+	protected $name = 'group';
 	protected $supportedContainers = array(
 		'units'		=> 'XliffUnit'
     );
 }
 
-class XliffUnit extends XliffElement{
-	protected $source , $target;
+
+
+/**
+ * Enter description here ...
+ * @author oyagev
+ * @method XliffNode source()
+ * @method XliffNode target()
+ */
+class XliffUnit extends XliffNode{
+	protected $name = 'trans-unit';
+	protected $supportedNodes = array(
+		'source' => 'XliffNode',
+		'target' => 'XliffNode',
+	);
 }
 
 
